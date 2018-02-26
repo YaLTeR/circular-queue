2ba9b058c9df8c29939e0159b86a8b2114f6c003
Ricky Han <rickylqhan@gmail.com>
2018-02-26 03:13:45 -0500

    add pop

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,7 @@
 //! assert_eq!(iter.next(), Some(&2));
 //! ```
 
+use std::ptr;
 use std::iter::{Chain, Rev};
 use std::slice::{Iter as SliceIter, IterMut as SliceIterMut};
 
@@ -36,6 +37,7 @@ pub struct CircularQueue<T> {
     // zero-sized types.
     capacity: usize,
     insertion_index: usize,
+    reverse_idx: usize,
 }
 
 /// An iterator over `CircularQueue<T>`.
@@ -68,6 +70,7 @@ impl<T> CircularQueue<T> {
             data: Vec::with_capacity(capacity),
             capacity,
             insertion_index: 0,
+            reverse_idx: 0,
         }
     }
 
@@ -173,8 +176,27 @@ impl<T> CircularQueue<T> {
         } else {
             self.data[self.insertion_index] = x;
         }
-
         self.insertion_index = (self.insertion_index + 1) % self.capacity();
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.data.len() == 0 {
+            None
+        } else {
+            if self.data.len() < self.capacity() {
+                self.data.pop()
+            } else {
+                self.reverse_idx += 1;
+                if self.reverse_idx > self.capacity() { None }
+                else {
+                    self.insertion_index += self.capacity - 1;
+                    self.insertion_index %= self.capacity;
+                    unsafe {
+                        Some(ptr::read(self.data.get_unchecked(self.insertion_index)))
+                    }
+                }
+            }
+        }
     }
 
     /// Returns an iterator over the queue's contents.
@@ -318,6 +340,31 @@ mod tests {
 
         let res: Vec<_> = q.iter().map(|&x| x).collect();
         assert_eq!(res, [3, 2, 1]);
+    }
+
+    #[test]
+    fn popping() {
+        let mut q = CircularQueue::with_capacity(5);
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+        q.push(5);
+        q.push(6);
+        q.push(7);
+
+        let res = q.pop();
+        assert_eq!(res, Some(7));
+        let res = q.pop();
+        assert_eq!(res, Some(6));
+        let res = q.pop();
+        assert_eq!(res, Some(5));
+        let res = q.pop();
+        assert_eq!(res, Some(4));
+        let res = q.pop();
+        assert_eq!(res, Some(3));
+        let res = q.pop();
+        assert_eq!(res, None);
     }
 
     #[test]
