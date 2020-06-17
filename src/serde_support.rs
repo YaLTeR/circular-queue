7b7d11e003bfe051a3dd5493a011b6c7b56b4a4d
Samit Basu <samit.basu@smiths.com>
2020-06-16 17:33:30 -0700

    Apply cargo fmt.  Remove pub from serde visitor struct.

--- src/serde_support.rs
+++ src/serde_support.rs
@@ -3,15 +3,20 @@ extern crate serde_test;
 
 use super::*;
 
-use self::serde::{Deserialize, Deserializer};
 use self::serde::de::{SeqAccess, Visitor};
 use self::serde::export::fmt;
 use self::serde::export::PhantomData;
-use self::serde::ser::{Serialize, Serializer, SerializeSeq};
+use self::serde::ser::{Serialize, SerializeSeq, Serializer};
+use self::serde::{Deserialize, Deserializer};
 
-impl<T> Serialize for CircularQueue<T> where T: Serialize {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where
-        S: Serializer {
+impl<T> Serialize for CircularQueue<T>
+where
+    T: Serialize,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
         let mut seq = serializer.serialize_seq(Some(self.capacity()))?;
         for e in self.asc_iter() {
             seq.serialize_element(e)?;
@@ -22,21 +27,21 @@ impl<T> Serialize for CircularQueue<T> where T: Serialize {
 
 #[derive(Debug)]
 /// `serde::de::Visitor` for a circular queue
-pub struct CircularQueueVisitor<T> {
+struct CircularQueueVisitor<T> {
     marker: PhantomData<fn() -> CircularQueue<T>>,
 }
 
 impl<T> CircularQueueVisitor<T> {
     pub fn new() -> CircularQueueVisitor<T> {
         CircularQueueVisitor {
-            marker: PhantomData
+            marker: PhantomData,
         }
     }
 }
 
-
 impl<'de, T> Visitor<'de> for CircularQueueVisitor<T>
-    where T: Deserialize<'de>,
+where
+    T: Deserialize<'de>,
 {
     type Value = CircularQueue<T>;
 
@@ -46,7 +51,8 @@ impl<'de, T> Visitor<'de> for CircularQueueVisitor<T>
 
     #[inline]
     fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error>
-        where S: SeqAccess<'de>,
+    where
+        S: SeqAccess<'de>,
     {
         let mut values = CircularQueue::with_capacity(seq.size_hint().unwrap_or(0));
         while let Some(value) = seq.next_element()? {
@@ -57,10 +63,12 @@ impl<'de, T> Visitor<'de> for CircularQueueVisitor<T>
 }
 
 impl<'de, T> Deserialize<'de> for CircularQueue<T>
-    where T: Deserialize<'de>,
+where
+    T: Deserialize<'de>,
 {
     fn deserialize<D>(deserializer: D) -> Result<CircularQueue<T>, D::Error>
-        where D: Deserializer<'de>,
+    where
+        D: Deserializer<'de>,
     {
         deserializer.deserialize_seq(CircularQueueVisitor::new())
     }
@@ -78,22 +86,22 @@ mod tests {
         q.push(1);
         q.push(2);
 
-        assert_tokens(&q, &[
-            Token::Seq { len: Some(8) },
-            Token::I32(1),
-            Token::I32(2),
-            Token::SeqEnd,
-        ]);
+        assert_tokens(
+            &q,
+            &[
+                Token::Seq { len: Some(8) },
+                Token::I32(1),
+                Token::I32(2),
+                Token::SeqEnd,
+            ],
+        );
     }
 
     #[test]
     fn serialization_with_empty_queue() {
         let q = CircularQueue::<()>::with_capacity(0);
 
-        assert_tokens(&q, &[
-            Token::Seq { len: Some(0) },
-            Token::SeqEnd,
-        ]);
+        assert_tokens(&q, &[Token::Seq { len: Some(0) }, Token::SeqEnd]);
     }
 
     #[test]
@@ -108,13 +116,16 @@ mod tests {
         q.push(5);
         // At this point, the queue should be [2, 3, 4, 5]
 
-        assert_tokens(&q, &[
-            Token::Seq { len: Some(4) },
-            Token::I32(2),
-            Token::I32(3),
-            Token::I32(4),
-            Token::I32(5),
-            Token::SeqEnd,
-        ]);
+        assert_tokens(
+            &q,
+            &[
+                Token::Seq { len: Some(4) },
+                Token::I32(2),
+                Token::I32(3),
+                Token::I32(4),
+                Token::I32(5),
+                Token::SeqEnd,
+            ],
+        );
     }
 }
