ef3dd13d72c1ac2de5716dd64177a3563ebb36a5
Ivan Molodetskikh <yalterz@gmail.com>
2025-03-09 10:07:40 +0300

    Fix warnings on new Rust

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 extern crate version_check;
 
 fn main() {
+    println!("cargo:rustc-check-cfg=cfg(has_extern_crate_alloc)");
     if version_check::is_min_version("1.36.0").unwrap_or(false) {
         println!("cargo:rustc-cfg=has_extern_crate_alloc");
     }
