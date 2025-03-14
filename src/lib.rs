7e124c0859b80389c3ca3d44099863bc40826cc1
Ivan Molodetskikh <yalterz@gmail.com>
2025-03-14 10:52:03 +0300

    Bump version to 0.2.7

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@
 //! ```
 
 #![cfg_attr(has_extern_crate_alloc, no_std)]
-#![doc(html_root_url = "https://docs.rs/circular-queue/0.2.6")]
+#![doc(html_root_url = "https://docs.rs/circular-queue/0.2.7")]
 
 #[cfg(has_extern_crate_alloc)]
 extern crate alloc;
